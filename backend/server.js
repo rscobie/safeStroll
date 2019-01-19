75964afabf1dcca4e770cac90edd1de1e76833bc
Rory <scobier@email.arizona.edu>
2019-01-19 03:16:22 -0700

    added basic server code

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
+var data = fs.readFileSync('./test.csv').toString();
+var app = express();
+app.use(bodyParser.json({extended: true}));
+
+console.log("server starting");
+
+app.get('/test',function(req,res){
+	console.log("got here");
+	return res.status(200).json({"message": "connected"});
+});
+
+app.post('/get_route',function(req,res){
+    console.log(data)
+    app.status(200).json({'points': data});
+});
+
+app.listen(9190);//TODO: make this come from a file
+
+module.exports = app;
