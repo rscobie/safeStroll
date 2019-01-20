57a5a7538d60d94aec64e03f0426d4408e473ad4
Rory <scobier@email.arizona.edu>
2019-01-20 02:50:43 -0700

    .

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ var request = require('request');
 //was going to implement our own but turns out somebody made one already
 //no need to reinvent the wheel
 var KdTreePkg = require('kd-tree-javascript');
-kdTreePkg();//execute this
+console.log(kdTreePkg);
+KdTreePkg();//execute this
 
 //generate graph and kd-tree here
 var rawData = fs.readFileSync('../data/combineddata.json').toString().replace(/'/g,'"');
