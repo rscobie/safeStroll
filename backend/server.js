352946aa623599cd01a36d09cc4135c50e9b8e8b
Rory <scobier@email.arizona.edu>
2019-01-19 18:16:33 -0700

    .

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ rawData = JSON.parse(rawData);
 var app = express();
 app.use(bodyParser.json({extended: true}));
 app.set('views', '../frontend/SafeStroll');
-app.engine('html', function(){});
+app.set('view engine', 'html');
 console.log("server starting");
 
 /*
